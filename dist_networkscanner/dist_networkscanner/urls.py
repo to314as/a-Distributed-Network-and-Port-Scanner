51b3ff34b305ffd65435d6f74ec07e1bcecd4fe4
to314as <tobi20083@gmail.com>
2019-10-06 20:14:28 +0200

    template

--- dist_networkscanner/dist_networkscanner/urls.py
+++ dist_networkscanner/dist_networkscanner/urls.py
@@ -1,4 +1,4 @@
-"""networkscanner URL Configuration
+"""dist_networkscanner URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.output,name='output'),
-    path('', views.sender,name='sender')
 ]
