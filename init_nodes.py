61285b662b271037e1d759e9850205bbb8935f23
to314as <tobi20083@gmail.com>
2019-10-12 13:15:23 +0200

    setup_victimServer

--- init_nodes.py
+++ init_nodes.py
@@ -1,29 +1,36 @@
 import os
 import random
+import numpy as np
 amount_of_nodes=2
 existing_ports=65535
 amount_of_ports=5
 
 #create docker images
-os.chdir('/home/tobias/networkscanner/victim')
+os.chdir('/home/tobias/networkscanner/victim') #adjust to your path
 os.system('docker build -t v .')
-os.chdir('/home/tobias/network')
-os.system('docker build -t n .')
 
 #a function to define a random subset of open ports on the victim machine
 def open_ports(n):
   open_ports=''
+  ports=[]
   for i in range(0,n):
-    p=random.randint(1,existing_ports)
-    open_ports+=f'-p {p}:{p} '
-  print(open_ports)
-  return open_ports
+    p=int(random.random()*existing_ports)
+    ports.append(p)
+    open_ports+=f'-p 127.0.0.1:{p}:{p} '
+  ports=np.array(ports)
+  np.save('victim_ports',ports)
+  return open_ports,ports
  
 #create the victim server with a given number of open ports
-open_ports=open_ports(amount_of_ports) 
-os.system(f'docker run '+open_ports+' --name victim v')
+open_ports_string=open_ports(amount_of_ports)[0]
+open_ports=open_ports(amount_of_ports)[1]
+print(open_ports)
+os.system(f'docker run '+open_ports_string+f' --name victim v python3 setup_server.py {open_ports} &')
 
 #create the attacker nodes
+os.chdir('/home/tobias/networkscanner/network') #adjust to your path
+os.system('docker build -t n .')
+
 for i in range(amount_of_nodes):
             os.system(f'docker run -p {5000 + i}:5000 --name n{i} n python job_processor.py container_{i} &')
             
---
+++ victim/setup_server.py
@@ -0,0 +1,25 @@
+import sys
+import socket
+import select
+import numpy as np
+
+if len(sys.argv) <= 1:
+    ports = 'default_container_name'
+else:
+    ports = sys.argv[1][1:-1].split(',')
+
+HOST = '127.0.0.1'
+servers = []
+for port in ports:
+    print(port)
+    ds = (HOST, int(port))
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(ds)
+    server.listen(10)
+    servers.append(server)
+
+while True:
+    readable,_,_ = select.select(servers, [], [])
+    ready_server = readable[0]
+    connection, address = ready_server.accept()
